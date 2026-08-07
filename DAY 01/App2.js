db67c089485e0dfd988c058da27e3c0a3002382c
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2026-08-07 13:53:48 +0530

    Files

---
+++ DAY 01/App2.js
@@ -0,0 +1,6 @@
+const heading = React.createElement("h1", {}, "Hello world!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
+
+const heading = React.createElement("div", {id:"parent"}), ;
