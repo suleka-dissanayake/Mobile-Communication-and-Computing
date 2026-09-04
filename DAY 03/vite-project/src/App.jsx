eaef9a9c110d15a56a6a11545a6f824c5e470397
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-04 11:50:17 +0530

    Files

--- DAY 03/vite-project/src/App.jsx
+++ DAY 03/vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 import FunctionalComponent from './component/functionalComponent.jsx'
 import { useState } from 'react'
 import './App.css'
+import GreetClass from './component/GreetClass.jsx'
 
 function App() {
   return (
-    <FunctionalComponent />
+      //<FunctionalComponent />
+      <GreetClass />
+    
   )
 }
 
--- DAY 03/vite-project/src/component/GreetClass.jsx
+++ DAY 03/vite-project/src/component/GreetClass.jsx
@@ -1,7 +1,7 @@
-class GreetClass extends React.Component {
+import React from 'react';
+
+export default class GreetClass extends React.Component {
     render() {
         return <h1>Hello, welcome to my class component!</h1>;
     }
 }
-
-export default GreetClass;
