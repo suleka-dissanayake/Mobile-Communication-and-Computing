8d4f88fb47bb06d44a6c7924f06e993a6f9484b1
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-04 12:19:09 +0530

    Files (have some errors)

--- DAY 03/vite-project/src/component/FunctionalComponent.jsx
+++ DAY 03/vite-project/src/component/FunctionalComponent.jsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
 export default function FunctionalComponent(props) {
     return <h1>Hello, {props.name}! Welcome to my functional component!</h1>;
+}
+
+export default function Greeting(props) {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <h1 className='bg'>Hello {props.name}</h1>
+            <button onclick={()=>setCount(count+1)}>{count}</button>
+        </>
+    );
 }
