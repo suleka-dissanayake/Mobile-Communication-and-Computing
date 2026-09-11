5c1a94efe43370bb0b39af790b6c71c6750928c1
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-11 11:07:55 +0530

    Add StudentCom component and update App to render it

--- DAY 03/src/App.jsx
+++ DAY 03/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import FunctionalComponent from './component/functionalComponent.jsx'
 import GreetClass from './component/GreetClass.jsx'
+import StudentCom from './component/studentCom.jsx'
 
 function App() {
   return (
-      <FunctionalComponent name="Suleka" />
+      //<FunctionalComponent name="Suleka" />
      // <GreetClass name="Suleka" />
+     <StudentCom />
   )
 }
 
---
+++ DAY 03/src/component/StudentCom.jsx
@@ -0,0 +1,7 @@
+export default function StudentCom() {
+    return (
+        <>
+            <h1 className='bg'>Hello Student</h1>
+        </>
+    );
+}
