be9a93db1793b4087b6ddccacfe3c8a2c18d25d2
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-11 12:11:41 +0530

    Files

--- DAY-03/src/component/StudentCom.jsx
+++ DAY-03/src/component/StudentCom.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function StudentCom(props) {
+export default function StudentCom(name, course, age) {
+    const [studentName, setStudentName] = useState(name);
     return (
         <div>
             <h2> Student Information</h2>
-            <p>Name: {props.name}</p>
-            <p>Course: {props.course}</p>
+            <p>Name: {name}</p>
+            <p>Course: {course}</p>
+            <p>Age: {age}</p>
         </div>
     );
 }
