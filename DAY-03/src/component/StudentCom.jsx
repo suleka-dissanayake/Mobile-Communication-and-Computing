836a5a8eaf609fc19b28711daf7cf41f2710a0e1
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-11 12:23:29 +0530

    Files

--- DAY-03/src/component/StudentCom.jsx
+++ DAY-03/src/component/StudentCom.jsx
@@ -3,12 +3,21 @@ import { useState } from 'react';
 
 export default function StudentCom(name, course, age) {
     const [studentName, setStudentName] = useState(name);
+    const [visible, setVisible] = useState(true);
+
+    const showName = () => {
+        alert(`Student Name: ${studentName}`);
+    }
+    
     return (
         <div>
             <h2> Student Information</h2>
+            <button onClick={showName}>Show Name</button>
             <p>Name: {name}</p>
             <p>Course: {course}</p>
             <p>Age: {age}</p>
+
+            (visible && <h1>{studentName}</h1>)
         </div>
     );
 }
