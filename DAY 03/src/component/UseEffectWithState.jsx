f0f8716b39b773d7819b6074c23b72a25726c3b5
suleka-dissanayake <sulekadissanayake2003@gmail.com>
2026-09-23 16:26:19 +0530

    Files

--- DAY 03/src/component/UseEffectWithState.jsx
+++ DAY 03/src/component/UseEffectWithState.jsx
@@ -11,7 +11,9 @@ export default function UseEffectWithState() {
     return (
         <div>
             <h1>Count: {count}</h1>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count + 1)}>
+                Count: {count}
+            </button>
         </div>
     );
 }
